435a322d92baeb150d3aadcf6689a7efea50d5ed
kamil <kamillev@wp.pl>
2019-11-13 04:15:57 +0100

    dodaneraz

--- src/test/java/com/lewandowski/aplikacjabazodanowa/AplikacjaBazoDanowaApplicationTests.java
+++ src/test/java/com/lewandowski/aplikacjabazodanowa/AplikacjaBazoDanowaApplicationTests.java
@@ -1,13 +1,21 @@
 package com.lewandowski.aplikacjabazodanowa;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.lewandowski.aplikacjabazodanowa.domain.Actor;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class AplikacjaBazoDanowaApplicationTests {
+public class AplikacjaBazoDanowaApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads()  {
+
     }
 
 }
